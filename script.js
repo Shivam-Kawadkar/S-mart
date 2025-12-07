9d8f13c9efcf8d324ef385598e02a62c573e3646
Shivam <skawadkar07@gmail.com>
2025-12-07 14:06:27 +0530

    Refactor HTML structure and improve styling
    - Updated HTML for better readability and organization, including consistent indentation and spacing.
    - Enhanced the navbar layout and added hover effects for buttons.
    - Improved search functionality in script.js for better performance and readability.
    - Added cursor pointer style to buttons and links for better user experience.
    - Adjusted CSS styles for product cards, including hover effects and box shadows for a more modern look.
    - Ensured consistent styling across various elements, including categories and promotional sections.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const search =()=> {
+const search = () => {
     const searchboxx = document.getElementById("searchbox").value.toUpperCase();
     const storeitems = document.getElementById("products");
     const product = document.querySelectorAll(".pro-card");
     const pname = storeitems.getElementsByTagName("p");
 
-    for(var i=0; i< pname.length; i++) {
+    for (var i = 0; i < pname.length; i++) {
         let match = product[i].getElementsByTagName('p')[0];
-        if(match) {
+        if (match) {
             let textvalue = match.textContent || match.innerHTML;
-            if(textvalue.toUpperCase().indexOf(searchboxx) > -1) {
+            if (textvalue.toUpperCase().indexOf(searchboxx) > -1) {
                 product[i].style.display = "";
             } else {
                 product[i].style.display = "none";
-            }   
+            }
         }
-    }   
+    }
 }
 
 function OpenProductPage() {
     window.location.href = "Product.html";
 }
-window.onload = function() {
+window.onload = function () {
     document.getElementById("searchbox").focus();
 };
